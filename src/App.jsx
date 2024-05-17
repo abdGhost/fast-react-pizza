7a6bb45b9b9bc4ec69043ad9af06e0a9f3c5c689
Abdul Taher Khan <49634197+abdGhost@users.noreply.github.com>
2024-05-17 10:52:35 +0530

    added create new order

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,41 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./ui/Home";
+import Error from "./ui/Error";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
-import CreateOrder from "./features/order/CreateOrder";
+import CreateOrder, {
+  action as createOrderAction,
+} from "./features/order/CreateOrder";
 import Order from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
+import { getOrder } from "./services/apiRestaurant";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       { path: "/", element: <Home /> },
-      { path: "/menu", element: <Menu />, loader: menuLoader },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      },
       { path: "/cart", element: <Cart /> },
-      { path: "/order/new", element: <CreateOrder /> },
-      { path: "order/:orderId", element: <Order /> },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+        action: createOrderAction,
+      },
+      {
+        path: "order/:orderId",
+        element: <Order />,
+        loader: async ({ params }) => {
+          return getOrder(params.orderId);
+        },
+        errorElement: <Error />,
+      },
     ],
   },
 ]);
