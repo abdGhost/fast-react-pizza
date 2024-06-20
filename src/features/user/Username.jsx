df8ae830b59dd0eb0513c2365cfbd2a014adac9d
Abdul Taher Khan <49634197+abdGhost@users.noreply.github.com>
2024-06-20 10:18:49 +0530

    added styling using flex

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,3 +1,3 @@
 export default function Username() {
-  return <div className="text-sm font-semibold hidden">Jonas</div>;
+  return <div className="text-sm font-semibold hidden md:block">Jonas</div>;
 }
